d2053e235d411a094d10b0ed464674804ee4f04a
20106 <royjin030709@gmail.com>
2022-10-27 18:38:49 -0400

    Draft

--- src/main/java/entities/Report.java
+++ src/main/java/entities/Report.java
@@ -1,6 +1,28 @@
 package entities;
 
+import java.lang.reflect.Type;
+
 public class Report {
+    public void check_counts(User a, Report b) {
+        if (b instanceof Type_A_Report){
+
+        }
+        return;
+    }
+};
+
+class Type_A_Report {
     private int report_count;
+
+    private void punish(User a){return;}
+
+    public void check_counts(User a, Report b) {
+        if (report_count == 3) {
+            punish(a);
+        }
+        return;
+    }
+
+    public void type_A() {return;}
 
 }
