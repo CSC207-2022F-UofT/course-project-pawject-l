21c5b26b4774a1c2258d9ef42a46af5e3abad6ba
LG Gram <LG@Gram>
2022-10-27 17:54:21 -0400

    Created Chat entity and added functions to the Text entity

--- src/main/java/entities/Text.java
+++ src/main/java/entities/Text.java
@@ -2,16 +2,44 @@ package entities;
 import java.time.LocalDateTime;
 
 public class Text {
+    private String senderID;
+    private LocalDateTime timeSent;
+    private String messageText;
+
     /**
      *A constructor for a text object.
+     *
+     * @param petID the ID of the pet who sent the text
+     * @param timeSent the date and time the text was sent
+     * @param messageText the message in the text
      */
-    final String senderID;
-    final LocalDateTime timeSent;
-    final String messageText;
-
     public Text(String petID, LocalDateTime timeSent, String messageText){
         this.senderID = petID;
         this.timeSent = timeSent;
         this.messageText = messageText;
+    }
+
+    /**
+     * A method that gets the ID of the text's sender
+     * @return the petID of the text's sender
+     */
+    public String getSenderID() {
+        return this.senderID;
+    }
+
+    /**
+     * A method that gets the time and date the text was sent
+     * @return the date and time the text was sent
+     */
+    public LocalDateTime getTimeSent() {
+        return this.timeSent;
+    }
+
+    /**
+     * A method that gets the message of the text
+     * @return the message of the text
+     */
+    public String getMessageText() {
+        return this.messageText;
     }
 }
