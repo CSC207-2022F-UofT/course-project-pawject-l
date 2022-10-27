049a7bc433dbcc10155771fb381bd81988c01e56
OsTeNg24 <otsgibson@gmail.com>
2022-10-27 18:32:42 -0400

    Quick Changes To FPMA

--- src/main/java/userCase/FPMA.java
+++ src/main/java/userCase/FPMA.java
@@ -1,18 +1,24 @@
 package userCase;
 import entities.Pet;
 import entities.Attributes;
+import org.w3c.dom.Attr;
 import repo.FPMARequestModel;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class FPMA implements FPMARequestModel {
     public Pet[] PotentialCandidates(Pet userPet)
     {
+        Dictionary potentialCandidateGrading = new Hashtable();
         Attributes userPetAttributes = userPet.getAttributes();
         Pet[] PossibleCandidates = getPossibleCandidates(userPet);
         for(Pet pet : PossibleCandidates)
         {
             Attributes candidatePetAttributes = pet.getAttributes();
-
+            potentialCandidateGrading.put(pet, getGrade(userPetAttributes, candidatePetAttributes));
         }
     }
     public Pet[] getPossibleCandidates(Pet userPet)
@@ -23,5 +29,8 @@ public class FPMA implements FPMARequestModel {
 
 
     }
+    public float getGrade(Attributes userPetAttributes, Attributes candidatePetAttributes)
+    {
 
+    }
 }
