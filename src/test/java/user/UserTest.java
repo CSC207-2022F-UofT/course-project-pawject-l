69acbf0cfe7147fe416474d798eb56597f3d277a
Elyse Ando <elysekando@gmail.com>
2022-11-12 15:32:18 -0500

    Fixed tests

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -1,53 +1,50 @@
 package user;
 
 import userCase.AccountModel;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class UserTest {
 
     @Test
     public void passwordWorks() {
-        AccountModel user = new AccountModel("elyse", "Javabae1!");
-        Assertions.assertEquals(true, AccountModel.checkPasswordValid(user.password));
+        AccountModel user = new AccountModel("User", "HelloWorld1");
+        assertTrue(AccountModel.checkPasswordValid(user.password));
     }
 
     @Test
     public void passwordFails() {
-        AccountModel user = new AccountModel("elyse", "111111111");
-        Assertions.assertEquals(false, AccountModel.checkPasswordValid(user.password));
+        AccountModel user = new AccountModel("User", "111111111");
+        assertFalse(AccountModel.checkPasswordValid(user.password));
     }
 
     @Test
     public void createUserTest1() {
-        AccountModel user = new AccountModel("elyse", "Javabae1!");
+        AccountModel user = new AccountModel("User", "HelloWorld1");
         user.createUser();
-        Assertions.assertEquals(true, AccountModel.userPassDict.containsKey("elyse"));
+        assertTrue(AccountModel.userPassDict.containsKey(user.username));
     }
 
     @Test
     public void userExistsTest() {
-        AccountModel user1 = new AccountModel("user1", "Javabae1!");
-        user1.createUser();
-        Assertions.assertEquals(true, AccountModel.userExists(user1.username));
+        AccountModel user = new AccountModel("User", "HelloWorld1");
+        user.createUser();
+        assertTrue(AccountModel.userExists(user.username));
     }
 
     @Test
     public void correctPasswordWorks() {
-        AccountModel user1 = new AccountModel("user1", "Javabae1!");
-        user1.createUser();
-        Assertions.assertEquals(true, user1.correctPassword("Javabae1!"));
+        AccountModel user = new AccountModel("User", "HelloWorld1");
+        user.createUser();
+        assertTrue(user.correctPassword(user.password));
     }
+
     @Test
     public void correctPasswordFails() {
-        AccountModel user1 = new AccountModel("user1", "Javabae1!");
-        user1.createUser();
-        Assertions.assertEquals(false, user1.correctPassword("Javabae1"));
+        AccountModel user = new AccountModel("User", "HelloWorld1");
+        user.createUser();
+        assertFalse(user.correctPassword("test"));
     }
-
-
-
 }
