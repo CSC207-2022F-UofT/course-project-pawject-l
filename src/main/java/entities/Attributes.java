33696be16c632654352684bb7a719efa2a171650
shirleywu915 <shirleyyt.wu@mail.utoronto.ca>
2022-11-12 20:50:00 -0500

    Updated some variable in Attributes.java and Pet.java. And added methods to ProfileRequest.java

--- src/main/java/entities/Attributes.java
+++ src/main/java/entities/Attributes.java
@@ -4,6 +4,7 @@ package entities;
 
 import java.time.DayOfWeek;
 import java.util.List;
+import java.awt.*;
 
 public class Attributes {
 
@@ -12,18 +13,20 @@ public class Attributes {
     private List<Integer> age; // allow for a range of preferred ages
     private String gender;
     private int preferredProximity; // distance in KM
+    private Image proofOfVaccination;
     private boolean vaccineStatus;
     private int longitude;
     private int latitude;
 
     private List<DayOfWeek> availableDay; // list of the available days of the week
 
-    public Attributes(List<String> species, List<String> breed, List<Integer> age, String gender, int preferredProximity,
-                      boolean vaccineStatus, int longitude, int latitude, List<DayOfWeek> availableDay){
+    public Attributes(List<String> species, List<String> breed, List<Integer> age, String gender, Image proofOfVaccination,
+                      boolean vaccineStatus, int longitude, int latitude, int preferredProximity, List<DayOfWeek> availableDay){
         this.species = species;
         this.breed = breed;
         this.age = age;
         this.gender = gender;
+        this.proofOfVaccination = proofOfVaccination;
         this.vaccineStatus = vaccineStatus;
         this.longitude = longitude;
         this.latitude = latitude;
@@ -44,6 +47,10 @@ public class Attributes {
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    public void setProofOfVaccination(Image proofOfVaccination){
+        this.proofOfVaccination = proofOfVaccination;
     }
 
     public void setVaccineStatus(boolean vaccine_status) {
@@ -78,6 +85,10 @@ public class Attributes {
 
     public String getGender() {
         return gender;
+    }
+
+    public Image getProofOfVaccination() {
+        return proofOfVaccination;
     }
 
     public boolean isVaccinated() {
