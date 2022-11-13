13bb12b47d75131aa1ecc7e7ff44ecdf3f4ac105
LG Gram <LG@Gram>
2022-11-13 13:26:19 -0500

    Changed functions for ChatDataRequestModel

--- src/main/java/repo/ChatDataRequestModel.java
+++ src/main/java/repo/ChatDataRequestModel.java
@@ -7,30 +7,25 @@ import java.util.HashMap;
 
 public interface ChatDataRequestModel {
 
-    Chat getChat(String id);
-
     /**
-     * Get all chats given a user.
-     * @param user
+     * Get all chats from the database
      * @return Hashmap with keys as the id and values as Chat object
      */
-    HashMap<String, Chat> getChatsByUser(User user);
+    HashMap<String, Chat> getChats();
 
     /**
-     * Get a chat given the user and the other user.
-     * @param currUser
-     * @param otherUser
-     * @return Pet
+     * Get a chat given the chatID.
+     * @param chatID chat ID
+     * @return Chat
      */
-    Chat getChatByUsers(User currUser, User otherUser);
+    Chat getChatByID(String chatID);
 
     /**
      * Saves chat in database. Returns true if saved, false if not.
-     * @param chat
+     * @param pet1ID, pet2ID, chat
      * @return true if saved, false if not
      */
-    boolean saveChat(Chat chat);
+    boolean saveChat(String pet1ID, String pet2ID, Chat chat);
 
-    // Let me know if you would like other methods (e.g. to get a specific chat).
 
 }
