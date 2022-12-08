aa404c88dcfa3c9b918d0c5b71e1349cec8ae4b6
20106 <royjin030709@gmail.com>
2022-12-08 14:41:14 -0500

    Draft

--- src/main/java/useCase/Report/ReportInputBoundary.java
+++ src/main/java/useCase/Report/ReportInputBoundary.java
@@ -5,7 +5,28 @@ import entities.Report;
 import java.io.IOException;
 
 public interface ReportInputBoundary {
+    /**
+     * Creates a Type A report.
+     *
+     * @param userID
+     * @throws IOException
+     */
     public void TypeAReport(String userID) throws IOException;
+
+    /**
+     * Creates a Type B report.
+     *
+     * @param petID
+     * @throws IOException
+     */
     public void TypeBReport(String petID) throws IOException;
+
+    /**
+     * Creates a Type C report.
+     *
+     * @param petID
+     * @param chatId
+     * @throws IOException
+     */
     public void TypeCReport(String petID, String chatId) throws IOException;
 }
